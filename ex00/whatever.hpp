44f84d1af94a1d5143423d2a48baecec57663b76
Raehyeon Jeong <raehy19@gmail.com>
2024-09-02 03:29:56 +0900

    feat : ex00 add comments

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -13,9 +13,13 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-#include <iostream>
-#include <string>
-
+/**
+ * Swaps the values of two variables.
+ *
+ * @tparam T Type of the variables.
+ * @param a First variable.
+ * @param b Second variable.
+ */
 template<typename T>
 void swap(T &a, T &b) {
 	T temp = a;
@@ -23,15 +27,30 @@ void swap(T &a, T &b) {
 	b = temp;
 }
 
+/**
+ * Returns the smaller of two values.
+ *
+ * @tparam T Type of the variables.
+ * @param a First value.
+ * @param b Second value.
+ * @return Reference to the smaller value.
+ */
 template<typename T>
 const T &min(const T &a, const T &b) {
 	return (a < b) ? a : b;
 }
 
+/**
+ * Returns the larger of two values.
+ *
+ * @tparam T Type of the variables.
+ * @param a First value.
+ * @param b Second value.
+ * @return Reference to the larger value.
+ */
 template<typename T>
 const T &max(const T &a, const T &b) {
 	return (a > b) ? a : b;
 }
 
 #endif
-
